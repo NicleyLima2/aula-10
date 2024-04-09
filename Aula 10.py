ea2e5619f8447d22d340d7bc775f80f71ca35d12
NicleyLima2 <161388917+NicleyLima2@users.noreply.github.com>
2024-04-08 21:14:58 -0300

    Add files via upload

---
+++ Aula 10.py
@@ -0,0 +1,87 @@
+
+# for ... in range (inicio, fim-1, incremento)
+
+# for numeros in range (0,21,2):
+#     print (numeros, end= "-")
+
+################
+
+# notas = []
+#
+# for solicitacao in range(0,6):
+#     nota = int(input('digite um numero:'))
+#     notas.append(nota)
+# print("a soma das notas é:", sum(notas))
+# print("a media das notas é:", sum(notas)/solicitacao)
+
+################
+
+# for numeros in range (0,51,2):
+#     print (numeros, end= "-")
+
+################
+# for numeros in range (1,51,2):
+#     print (numeros, end= "-")
+################
+#
+# numero = int(input('digite um numero qualquer:'))
+# multiplicador = 0
+# while multiplicador <= 10:
+#     resultado = numero * multiplicador
+#     print("{} * {} = {}".format(numero, multiplicador, resultado))
+#     multiplicador = multiplicador + 1
+
+################
+
+# numero_termos = int(input("digite o numero de termos:"))
+# n1 = 0
+# n2 = 1
+#
+# for ok in range (numero_termos):
+#     print (n1)
+#     variavel_temporaria = n1
+#     n1 = n2
+#     n2 = variavel_temporaria + n2
+
+################
+
+# numero = int(input("digite um numero qualquer:"))
+# fatorial = 1
+# if numero <0:
+#     print("Erro! Nao existe")
+# elif numero == 0:
+#     print ("fatorial de zero é 1")
+# else:
+#     for incremento in range(1,numero+1):
+#         fatorial = fatorial * incremento
+#     print("O fatorial de {} é {}". format(numero, fatorial))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################
+
+
+
+
+
+
+
+
+
+
+
+
